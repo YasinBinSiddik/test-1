dedc26b00f64ba05e078011200e0c9c38dac2a29
YasinBinSiddik <yasinarafat00005@gmail.com>
2023-04-20 15:37:15 +0600

    added php

---
+++ chatgpt.php
@@ -0,0 +1,49 @@
+<section id="hero">
+  <div class="hero-content">
+    <h1>Welcome to my Portfolio Website</h1>
+    <p>I am a web developer with experience in HTML, CSS, and JavaScript. Take a look at my portfolio to see my latest projects.</p>
+    <a href="#portfolio" class="btn">View Portfolio</a>
+  </div>
+</section>
+
+<section id="about">
+  <div class="container">
+    <h2>About Me</h2>
+    <p>Hi, I'm [Your Name], a web developer with [number] years of experience in developing responsive and user-friendly websites. I am skilled in HTML, CSS, and JavaScript, and have experience in using frameworks such as React and Angular.</p>
+    <p>I am passionate about creating websites that not only look great, but also provide a seamless user experience. Whether you need a simple brochure website or a complex web application, I can help you bring your ideas to life.</p>
+  </div>
+</section>
+
+<section id="services">
+  <div class="container">
+    <h2>Services</h2>
+    <p>Here are some of the services I offer:</p>
+    <ul>
+      <li>Website design and development</li>
+      <li>Responsive design</li>
+      <li>Search engine optimization (SEO)</li>
+      <li>Website maintenance</li>
+    </ul>
+  </div>
+</section>
+
+<section id="portfolio">
+  <div class="container">
+    <h2>Portfolio</h2>
+    <p>Here are some of my latest projects:</p>
+    <div class="project">
+      <img src="project-1.jpg" alt="Project 1">
+      <h3>Project 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn">View Project</a>
+    </div>
+    <div class="project">
+      <img src="project-2.jpg" alt="Project 2">
+      <h3>Project 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a href="#" class="btn">View Project</a>
+    </div>
+    <div class="project">
+      <img src="project-3.jpg" alt="Project 3">
+      <h3>Project 3</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
